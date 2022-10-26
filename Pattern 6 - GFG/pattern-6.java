841e880fc044f6a2f72a19b3669dbabf37a18dd5
tejasvita95 <48647830+tejasvita95@users.noreply.github.com>
2022-10-26 23:49:17 +0530

    Added solution - LeetHub

--- Pattern 6 - GFG/pattern-6.java
+++ Pattern 6 - GFG/pattern-6.java
@@ -21,7 +21,7 @@ class Solution {
 
     void printTriangle(int n) {
         for(int i=0; i<n; i++){
-            for(int j=0; j<=(n-i-1); j++){
+            for(int j=0; j<(n-i); j++){
                 System.out.print(j+1 + " ");
             }
             System.out.println();
